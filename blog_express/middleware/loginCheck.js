2c95c1a2473827f52f3bc30f11a17cc7292bce3f
秦振坤 <johnqin@rektec.com.cn>
2019-07-23 19:43:10 +0800

    初始化文件

--- blog_express/middleware/loginCheck.js
+++
@@ -1,9 +0,0 @@
-const { ErrorModel } = require('../model/resModel')
-
-module.exports = (req, res, next) => {
-  if (req.session.username) {
-    next()
-    return
-  }
-  res.send(new ErrorModel('未登录'))
-}
